f09dbc715542da9731aec3307de30448993a5ed5
kradworkz <80160702+kradworkz@users.noreply.github.com>
2024-10-16 09:26:01 +0800

    as

--- app/Services/Management/ProspectusClass.php
+++ app/Services/Management/ProspectusClass.php
@@ -2,10 +2,22 @@
 
 namespace App\Services\Management;
 
-use App\Models\SchoolCampusCourse;
+use App\Models\SchoolCampusCourseProspectus;
 
 class ProspectusClass
 {
+    public function save($request){
+        $years = $request->years;
+        $term = $request->term;
+        
+        $data = SchoolCampusCourseProspectus::create($request->all());
+        return [
+            'data' => $data,
+            'message' => 'School created successfully.', 
+            'info' => 'The new school has been added.',
+        ];
+    }
+
     public function view($request){
         $data = SchoolCampusCourse::with('course','campus.school','prospectuses')->first();
         return $data;
